d0a618198be0fbd91c697063f0d18bfe5b1537a9
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-03-11 08:38:04 -0300

    Create new file aula5-calculos.py; created in 07th  march. Explaining calculate.

---
+++ section1_basic/aula5-calculos.py
@@ -0,0 +1,23 @@
+print('07/03/2026 - Aula 05: Primeiros Cálculos em Python.')
+
+print('\n\nOperações matemáticas básicas\n')
+print(2 + 2)
+print(2 - 2)
+print(2 / 2)
+print(2 * 2)
+
+#*----------------------------------------------
+print('\n\nVenda de produto.\n')
+nomeComprador = 'Theo Augusto'
+produto = 'Celular'
+precoCell = 1500
+quantidade = 2
+total = quantidade * precoCell
+
+print(f'Nome do cliente: {nomeComprador}')
+print(f'Produto: {produto}')
+print(f'{quantidade}')
+print(f'Preço da unnidade: {precoCell}')
+print(f'Total a pagar: R$ {total}')
+
+print('\n')
