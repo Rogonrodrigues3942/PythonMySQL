125ce2a26e24d1462a52ae2a7ada594d5b6064b3
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-03-11 08:36:03 -0300

    Add new file, subject concat strings. Created in 07 march.

---
+++ section1_basic/aula4-cariaveis-concatenacao.py
@@ -0,0 +1,11 @@
+print('Aula 04 - Variáveis e Concatenação na Prática.')
+
+nome =  'Wagner Cardoso'
+idadeCliente = 41
+profissaoCliente = 'Analista de Sistemas'
+atualSalario = 6400.29
+
+print('Nome do cliente: ', nome)
+print(f'Idade do cliente: {idadeCliente} anos.')
+print(f'Profissão do cliente: {profissaoCliente}')
+print(f'Renda: $ {atualSalario}')
