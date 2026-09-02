a2b7164bb383f2c79be558fa2ca6bfa78d12cb25
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-02 19:01:58 -0300

    Updating aula11-while-repetitioh.

--- section1_basic/aula11-while-repetition.py
+++ section1_basic/aula11-while-repetition.py
@@ -1,13 +1,26 @@
+
 print('\nAulas 11 e 12 - while - repetition structure.\n')
+
+toContinue = input('Deseja inserir mais um cadastro: (sim[s] ou  não[n]): ')
+
+if(toContinue == 'n' or toContinue == 'N'):
+    print('\nPrograma encerrado!\n')
+    exit()
+
 
 productName = input('Digite o nome do produto: ')
 quantity = int(input('Digite a quantidade; '))
-toContinue = input('Deseja inserir mais um cadastro: (sim[s] ou  não[n]): ')
 
-while(toContinue == 's'):
+while(toContinue == 's' or toContinue == 'S'):
     print(f'Produto: {productName}.')
+    print(f'Quantidade: {quantity}.')
     toContinue = input('Deseja inserir mais um cadastro: (sim[s] ou  não[n]): ')
+
+    if(toContinue == 'n' or toContinue == 'N'):
+        print('\nPrograma encerrado!\n')
+        exit()
+
     productName = input('Digite o nome do produto: ')
     quantity = int(input('Digite a quantidade; '))
-
+    
 print('\nFim do script.\n')
